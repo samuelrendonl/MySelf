6e793b046bf7717efad526afaaab41989a1c08f9
Samuel Rendón <samuellozanorendon@gmail.com>
2026-02-25 22:25:00 -0500

    estilizacion de los componentes

--- scripts.js
+++ scripts.js
@@ -1,5 +1,20 @@
-let boton = document.getElementById("button1");
+let boton1 = document.getElementById("button1");
+let boton2 = document.getElementById("button2");
+let boton3 = document.getElementById("button3")
+let boton4 = document.getElementById("button4")
 
-boton.addEventListener("click", function() {
-    alert("Hola Samuel 🚀");
-});
+boton1.addEventListener("click", function abrirPagina() {
+    window.open("https://github.com/samuelrendonl", "_blank");
+  });
+
+boton2.addEventListener("click", function abrirPagina(){
+    window.open("https://www.instagram.com/samuelrendonl/")
+})
+
+boton3.addEventListener("click", function abrirPagina(){
+    window.open("https://www.youtube.com/@samuelrendonlozano496")
+})
+
+boton4.addEventListener("click", function abrirPagina(){
+    window.open("https://www.tiktok.com/@samuelrendonl")
+})
